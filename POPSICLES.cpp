250bb857ae1d4b93bacd32ea3309c78050ead2a9
eFoxS <76924382+WhaTheFoxSay@users.noreply.github.com>
2023-01-02 08:16:38 +0700

    Create POPSICLES.cpp
    Tugas Mata Kuliah Logika Teknik Pemograman pada bahasa C++ Semester 1

---
+++ POPSICLES.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int siblings, popsicles;
+    //take input
+    cin>>siblings>>popsicles;
+
+    //your code goes here
+    
+    return 0;
+}
