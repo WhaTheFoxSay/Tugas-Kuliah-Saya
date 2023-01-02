129bac63d9d66b647b52ae85128a5f49f1c4dea9
eFoxS <76924382+WhaTheFoxSay@users.noreply.github.com>
2023-01-02 08:24:09 +0700

    Create Gotham-City.cpp

---
+++ Gotham-City.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int ferdinan, low, max;
+    cin >> ferdinan;
+    low = 5;
+    max = 10;
+  if (ferdinan < low) {
+    cout << "I got this!";
+  }
+    else if (ferdinan <= max) {
+      cout << "Help me Batman";
+  }
+  else {
+    cout << "Good Luck out there!";
+  }
+    return 0;
+}
