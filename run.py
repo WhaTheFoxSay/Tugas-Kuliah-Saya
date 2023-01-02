31b718e456b36adcb9ca0d2566cff2d0ea1dee45
eFoxS <76924382+WhaTheFoxSay@users.noreply.github.com>
2023-01-02 12:08:57 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,51 @@
+from CalculatorInan import *
+
+def signals(self):
+    self.pushButton_2.clicked.connect(self.calc)
+
+def calc(self):
+    a = int(self.lineEdit_1.text())
+    b = int(self.lineEdit_2.text())
+    operator = self.comboBox.currentText()
+    try:
+        if operator == "+":
+            result = a + b
+        elif operator == "-":
+            result = a - b
+        elif operator == "/":
+            result = a / b
+        elif operator == "*":
+            result = a * b
+        
+        #% percent
+        elif operator == "%":
+            result = a * ( b / 100)
+
+        #mod
+        elif operator == "mod":
+            result = a % b
+
+        #pangkat
+        elif operator == "^":
+            result = a ** b
+        
+        else:
+            result = "xx"
+        self.lineEdit_3.setText(str(result))
+            
+    except:
+        self.lineEdit_3.setText('Error!!!')
+    
+
+Ui_MainWindow.signals = signals
+Ui_MainWindow.calc = calc
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.signals()
+    MainWindow.show()
+    sys.exit(app.exec_())
